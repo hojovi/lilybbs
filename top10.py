36e8ef31718578061fbbfd2e01db2d8fa47f9608
hojovi <htjovi@gmail.com>
2016-05-24 00:25:38 +0800

    修改replace into语句，改成判断

--- top10.py
+++ top10.py
@@ -10,9 +10,17 @@ conn=pymysql.connect(host='127.0.0.1',port=3306,user='root',passwd='123456',db='
 cur=conn.cursor()
 
 try:
-    sql='replace into top10(boardId,post_url,title,sender,followers,end_time) values(%s,%s,%s,%s,%s,%s)'
+    select_sql='select count(*) from top10 where post_url=%s'
+    insert_sql='insert into top10(boardId,post_url,title,sender,followers,end_time) values(%s,%s,%s,%s,%s,%s)'
+    update_sql='update top10 set end_time=%s,followers=%s where post_url=%s'
     for a in top10:
-        cur.execute(sql,(board_index[a[0]],a[1],a[2],a[3],a[4],cur_time))
+        cur.execute(select_sql,(a[1],))
+        for count in cur:
+            if count[0]==0:
+                cur.execute(insert_sql,(board_index[a[0]],a[1],a[2],a[3],a[4],cur_time))
+            else:
+                cur.execute(update_sql,(cur_time,a[4],a[1]))
+    
     conn.commit()
 finally:
     cur.close()
--- topall.py
+++ topall.py
@@ -10,9 +10,16 @@ conn=pymysql.connect(host='127.0.0.1',port=3306,user='root',passwd='123456',db='
 cur=conn.cursor()
 
 try:
-    sql='replace into topall(boardId,post_url,title,end_time) values(%s,%s,%s,%s)'
+    select_sql='select count(*) from topall where post_url=%s'
+    insert_sql='insert into topall(boardId,post_url,title,end_time) values(%s,%s,%s,%s)'
+    update_sql='update topall set end_time=%s where post_url=%s'
     for a in topall:
-        cur.execute(sql,(board_index[a[2]],a[0],a[1],cur_time))
+        cur.execute(select_sql,(a[0],))
+        for count in cur:
+            if count[0]==0:
+                cur.execute(insert_sql,(board_index[a[2]],a[0],a[1],cur_time))
+            else:
+                cur.execute(update_sql,(cur_time,a[0]))
     conn.commit()
 finally:
     cur.close()
